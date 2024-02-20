774284ff2cdd29b71da722ce72d524b937c9b8a3
guilherme-kunsch <guilherme.kunsch@outlook.com>
2024-02-19 22:24:02 -0300

    finalizando component content

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,20 +1,24 @@
+import { ClipboardText  } from '@phosphor-icons/react'
+
 export function PageContent() {
   return (
     <div className="flex flex-col text-white items-center">
-      <div className="flex justify-between items-center w-[46rem]">
+      <div className="flex justify-between items-center w-[46rem] my-4">
         <div className="flex gap-2">
-          <p className="text-blue-500 font-bold">Tarefas criadas</p>
-          <span className="flex justify-center bg-gray-800 text-white w-7 h-6 rounded-full">1</span>
+          <p className="text-blue-400 font-bold">Tarefas criadas</p>
+          <span className="flex justify-center bg-gray-700 text-white w-7 h-6 rounded-full font-bold">0</span>
         </div>
-        <div>
-          <p>Concluídas</p>
-          <span>0</span>
+        <div className="flex gap-2">
+          <p className="text-purple-500 font-bold">Concluídas</p>
+          <span className="flex justify-center bg-gray-700 text-white w-7 h-6 rounded-full font-bold">0</span>
         </div>
       </div>
-      <div>
-        <span>Icone</span>
-        <h1>TEXTO</h1>
-        <h2>Texto</h2>
+      <div className="flex border-t border-gray-700 p-16 text-neutral-500 items-center flex-col w-[46rem] rounded-t-lg ">
+        <span>
+          <ClipboardText size={56}/>
+        </span>
+        <p className="font-bold mt-3">Você ainda não tem tarefas cadastradas</p>
+        <p>Crie tarefas e organize seus itens a fazer</p>
       </div>
     </div>
   );
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -3,7 +3,7 @@ export function ToAdd() {
   return (
     <div className="flex justify-center w-[46rem] mx-auto gap-2 relative -top-7">
       <input
-        className="bg-neutral-800 p-4 w-[40rem] rounded-lg text-white border border-black outline-none focus:border-purple-500 focus:border border-box"
+        className="bg-neutral-800 p-4 w-[40rem] rounded-lg text-white border border-black outline-none focus:border-purple-900 focus:border border-box"
         type="text"
         placeholder="Adicione uma nova tarefa"
       />
