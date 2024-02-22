ba989f13bf2981968fc57433b3ec56adeadbe8fe
guilherme-kunsch <guilherme.kunsch@outlook.com>
2024-02-22 09:03:14 -0300

    criando componente task

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,11 +1,18 @@
-import { CheckCircle } from '@phosphor-icons/react'
+import { CheckCircle, Trash } from "@phosphor-icons/react";
 
 export function CreatedTask() {
   return (
-    <div className='flex justify-center mx-auto gap-2 w-[46rem] border-2'>
-      <input type="radio" name="check" />
-      {/* <CheckCircle size={30} color='white' /> */}
-      <p className='text-gray-200'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat hic similique et iure, dicta nihil sequi, odit ab error commodi porro aliquid. Quis at voluptatum iste vel aperiam voluptatibus officiis.</p>
+    <div className="flex justify-center mx-auto gap-4 w-[46rem] bg-neutral-800 rounded-lg p-4">
+      <div className="flex flex-row items-start w-[40rem] gap-4 ">
+        <p className="text-gray-200">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, ipsa. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi natus molestias debitis vel vero accusantium asperiores incidunt dicta hic tempore laborum, architecto ad voluptatem blanditiis veritatis magni quia aliquam enim!
+        </p>
+      </div>
+      <div>
+        <button>
+          <Trash size={28} color="white" />
+        </button>
+      </div>
     </div>
-  )
+  );
 }
