f34fdc0ad05ffcca8932dc5be673a16110ef0429
guilherme-kunsch <guilherme.kunsch@outlook.com>
2024-09-17 17:39:15 -0300

    ajustando

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,9 @@
+export function Input() {
+    return (
+        <input
+        className="bg-neutral-800 p-4 w-[40rem] rounded-lg text-white border border-black outline-none focus:border-purple-900 focus:border border-box mb-2"
+        type="text"
+        placeholder="Adicione uma nova tarefa"
+      />
+    )
+}
--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -1,21 +1,16 @@
 import { PlusCircle } from "@phosphor-icons/react";
+import { Input } from "./Input/Input";
 
-export function NewTask({ onSubmit, newValue, setNewValue }) {
+export function NewTask() {
   return (
-    <div>
-    <form onSubmit={onSubmit} className="flex justify-center w-[46rem] mx-auto gap-2 relative -top-7">
-      <input
-        className="bg-neutral-800 p-4 w-[40rem] rounded-lg text-white border border-black outline-none focus:border-purple-900 focus:border border-box"
-        type="text"
-        placeholder="Adicione uma nova tarefa"
-        onChange={(event) => setNewValue(event.target.value)}
-        value={newValue}
-      />
-      <div className="flex justify-center items-center gap-2 p-4 text-white bg-blue-600 rounded-lg hover:bg-blue-500 cursor-pointer">
-        <button type="submit">Criar</button>
-        <PlusCircle size={18} />
-      </div>
-    </form>
+    <div className="flex justify-center w-[46rem] mx-auto gap-2 relative -top-7">
+      <form>
+        <div className="flex flex-col">
+          <Input />
+          <Input />
+        </div>
+      </form>
+      
     </div>
   );
 }
